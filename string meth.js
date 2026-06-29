54add52028e554a33aec38e59929af77b549f96a
ADISH  C P <adishadi7559@gmail.com>
2026-06-29 15:17:17 +0530

    Add files via upload

---
+++ string meth.js
@@ -0,0 +1,12 @@
+// REVERSE string
+// let str = "The quick brown fox";
+// let spl = str.split(" ");
+// rev = spl.reverse();
+// console.log(rev.join(" "));
+
+let str = "   ADMIN@mycompany.com  ";
+let clean = str.trim().toLowerCase();
+if(clean.startsWith("admin")){ 
+    console.log(clean);
+}
+
