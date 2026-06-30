889787554590b3e6b427216926ce55ecde65d896
ADISH  C P <adishadi7559@gmail.com>
2026-06-30 12:14:55 +0530

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,73 @@
+// const rgb = ['red','green','yellow'];
+// const [r,g,b] = rgb;
+// console.log(r);
+// console.log(g);
+// console.log(b);
+
+// const color = ['red','blue','green','yellow','orange'];
+// const [firstcol, ,thirdcol,forthcol] = color;
+// console.log(firstcol);
+// console.log(thirdcol);
+// console.log(forthcol);
+
+
+// const dimension = [1500];
+// const [width,height = 700] = dimension;
+// console.log(height);
+// console.log(width);
+
+
+// const planets = ['Earth','mercury','venus','neptune'];
+// const [MyPlanet,...RestPlan] = planets;
+// console.log(MyPlanet);
+// console.log(RestPlan);
+
+
+
+// const det = ['Adish',['Intern','Bridgeon']];
+// const [name,[position,institute]] = det;
+// console.log(name);
+// console.log(position);
+// console.log(institute);
+
+
+// let a = 1;
+// let b = 2;
+// [a,b] = [b,a];
+// console.log(a);
+// console.log(b);
+
+
+
+
+
+
+// function math(x,y){
+//     return [x+y,x*y];
+// }
+// const [sum,product] = math(5,10);
+// console.log(sum,product);
+
+
+            // Object destructuring
+
+// const det = {name:'Adish',age:21,place:'calicut'};
+// const {name,age,place} = det;
+// console.log(place);
+
+// const resp = {ad_id:2,ad_stat:'Verification success'};
+// const {ad_id:id,ad_stat:status} = resp;
+// console.log(id);
+// console.log(status);
+
+// const x = {theme:'love'};
+// const {theme,color ='red'} = x;
+// console.log(theme);
+// console.log(color);
+
+// const product = { id: 12, title: 'Mechanical Keyboard', price: 90, stock: 45 };
+// const {id,title,...shipdetails} = product;
+// console.log(id);
+// console.log(title);
+// console.log(shipdetails);
+
