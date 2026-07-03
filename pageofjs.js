ab5984d50d869463ee59d739a46dcd48a739bb49
ADISH  C P <adishadi7559@gmail.com>
2026-07-03 11:28:18 +0530

    Add files via upload

---
+++ pageofjs.js
@@ -0,0 +1,13 @@
+const head = document.createElement('h1');
+head.textContent="I am the best";
+head.style.color="blue";
+document.body.appendChild(head);
+const button = document.createElement('button');
+button.textContent="Click me";
+button.style.color="white";
+button.style.backgroundColor="red";
+document.body.appendChild(button);
+const divi = document.createElement('div')
+document.body.appendChild(divi);
+const text = document.createElement('input')
+document.body.appendChild(text);
