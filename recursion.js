e3fe2c4d4ffddd3f0dff141770989e8ad3a2849b
ADISH  C P <adishadi7559@gmail.com>
2026-07-07 12:55:54 +0530

    Add files via upload

---
+++ recursion.js
@@ -0,0 +1,24 @@
+// function countDown(num){
+//     if(num<=0){
+//         console.log("Blast off !");
+//         return;
+//     }
+//     console.log(num);
+//     countDown(num-1);
+
+// }
+// countDown(3)
+
+
+// function brokenCounter(n) {
+//     console.log(n);
+//     return brokenCounter(n + 1); // Getting further away from 0!
+// }
+// brokenCounter(2)
+
+
+// function fact(num){
+//     if(num===1) return 1;
+//     return num*fact(num-1);
+// }
+// console.log(fact(9));
